d518a7910386d07739f2fa596524882a53039b7c
Rishi Rajalingham <rishir@mh17.mit.edu>
2013-04-17 15:42:43 -0400

    adding feret database for face recognition and viewpoint regression tasks

--- simffa/simffa_feret.py
+++ simffa/simffa_feret.py
@@ -16,10 +16,21 @@ import numpy as np
 import tabular as tb
 import Image
 import random
+
+import hyperopt
+from hyperopt import base
+from pyll import scope
+
+import simffa_params as sp
+import simffa_mtDat as mtdat
+from simffa_utils import slm_h5
+
 import skdata as skd
 from skdata import larray
 from skdata.utils import download, extract, int_labels
 from skdata.utils.image import ImgLoader
+
+from dldata_classifier import train_scikits
 
 class FeretData(object):
 
@@ -45,14 +56,15 @@ class FeretData(object):
 
         img_filenames = sorted(os.listdir(self.home(self.SUBDIR)))
         img_filenames = [os.path.join(self.home(self.SUBDIR),x) for x in img_filenames]
-
+        pose_defs = {'ba':0, 'bb':50, 'bc':40, 'bd':25, 'be':15, 'fa':0, 'fb':0,
+                    'ql':-22.5, 'qr':22.5, 'hl':-67.5, 'hr':67.5, 'pl':-90,'pr':90}  
         meta = []
         for img_filename in img_filenames:
             img_data = open(img_filename, 'rb').read()
             sha1 = hashlib.sha1(img_data).hexdigest()
-            subject_id = os.path.split(img_filename)[1].split('.')[0][:5]
-            pose_id = os.path.split(img_filename)[1].split('.')[0][5:7]
-            
+            subject_id = int(os.path.split(img_filename)[1].split('.')[0][:5])
+            pose_tag = (os.path.split(img_filename)[1].split('.')[0][5:7])
+            pose_id = pose_defs[pose_tag]
             data = dict(filename=img_filename,
                         subject_id=subject_id,
                         pose_id=pose_id,
@@ -99,9 +111,6 @@ class FeretData(object):
         inds = range(tmp.shape[0])
         image_paths = [self.meta[ind]['filename'] for ind in inds]
         imgs = larray.lmap(ImgLoader(ndim=2, dtype='uint8', mode='L'), image_paths)
-
-        # imgs = larray.lmap(ImgLoader(ndim=2, shape=(400,400), dtype='uint8', mode='L'),
-                           # image_paths)
         labels = np.asarray([ (self.meta[ind]['subject_id'], self.meta[ind]['pose_id']) for ind in inds])
     
         IMGS = np.array(imgs)
@@ -109,10 +118,141 @@ class FeretData(object):
    
         return IMGS, LABELS
 
-
 class FERET(FeretData):
     URL = 'http://dicarlocox-datasets.s3.amazonaws.com/FERET.zip'
     SHA1 = '3666828effe1cc77b6297406f1e0f9799137068e'
     SUBDIR = 'FERET'
 
+class FERET_gray(FeretData):
+    URL = 'http://dicarlocox-datasets.s3.amazonaws.com/FERET_gray.zip'
+    SHA1 = '9c694eb2ae49022566c641a04e5eea776928869a'
+    SUBDIR = 'FERET_gray'
+
+## methods for bandit ##
+
+def get_features(X, config, verbose=False, dirname=None, fname_tag=None):
+    features = slm_h5(
+                    desc=config['desc'],
+                    X=X,
+                    basedir=dirname,
+                    name=fname_tag, 
+                    save=False) 
+    features = np.array(features)
+    return features
+
+def getFERETsplits(labels, seed, nClasses, num_splits):
+    label_id = 0
+    uniqueLabels = np.unique(labels[:,label_id])
+    nIm = np.array(labels).shape[0]
+    rng = np.random.RandomState(seed)
+    splits = {}
+
+    for split_id in range(num_splits):
+        splits['train_' + str(split_id)] = []
+        splits['test_' + str(split_id)] = []
+
+        for i in range(nClasses):
+            iMlbl_i = [j for j in range(nIm) if labels[j,label_id] == uniqueLabels[i]]
+            perm_i = rng.permutation(len(iMlbl_i))
+            perm = [iMlbl_i[p_i] for p_i in perm_i]
+            for ind in perm[:1]:
+                splits['train_' + str(split_id)].append(ind)
+            for ind in perm[1:]:
+                splits['test_' + str(split_id)].append(ind)
+    return splits
+
+def get_FERET_classification(features, labels, nClasses=36, num_splits=2):
+
+    splits = getFERETsplits(labels, np.random.randint(nClasses), nClasses, num_splits)
+    id_test_accuracy = 0
+    view_test_rsq = 0
+    for i in range(num_splits):
+        train_inds = np.array(splits['train_' + str(i)])
+        test_inds = np.array(splits['test_' + str(i)])
+        train_X = features[train_inds]
+        test_X = features[test_inds]
+
+        # id classification
+        train_y = labels[train_inds,0]
+        test_y = labels[test_inds,0]
+        train_Xy = (train_X, train_y)
+        test_Xy = (test_X, test_y)
+        result = train_scikits(train_Xy, test_Xy, 'libSVM')
+        test_accuracy = test_accuracy + result[1]['test_accuracy']
+
+        #viewpoint regression
+        train_y = labels[train_inds,1]
+        test_y = labels[test_inds,1]
+        train_Xy = (train_X, train_y)
+        test_Xy = (test_X, test_y)
+        result = train_scikits(train_Xy, test_Xy, 'pls.PLSRegression')
+        view_test_rsq = view_test_rsq + result[1]['test_rsquared']
+
+    test_accuracy = test_accuracy / num_splits
+    view_test_rsq = view_test_rsq / num_splits
+    return test_accuracy, view_test_rsq
+
+@scope.define
+def feret_bandit_evaluate(config=None):
+    dataset = FERET()
+    imgs,labels = dataset.get_images()
+    nIm = labels.shape[0]
+    print 'Loading ' + str(nIm) + ' FERET imgs...'
+    features = get_features(imgs, config, verbose=False)
+    
+    print 'Evaluating model on id classificaiton and viewpoint regression'
+    test_accuracy, view_test_rsq = get_FERET_classification(features, labels, nClasses=36, num_splits=2)
+    results = {}
+    results['id_accuracy'] = test_accuracy
+    results['view_rsq'] = view_test_rsq
+
+    record = {}
+    record['spec'] = config
+    record['results'] = results
+    record['attachments'] = {}
+    record['loss'] = 1 - (view_test_rsq + test_accuracy)/2
+    record['status'] = 'ok'
+    return record
+
+@scope.define
+def feret_bandit_evaluate2(config=None, stats=None):
+    dataset = mtdat.MTData_March082013()
+    imgs,labels = dataset.get_images(label_id)
+    nIm = labels.shape[0]
+    print 'Loading ' + str(nIm) + 'MTURK imgs...'
+    features = get_features(imgs, config, verbose=False)
+
+    print 'regressing model on neural labels'
+    regress_r2 = {}
+    neuralname = ['pl', 'ml', 'al']
+
+    for neural_i in range(len(neuralname)):
+        curr_neural = dataset.get_neural_labels(neural_i+1)
+        tmp = [get_regression_results(features, curr_neural[:,ei]) for ei in range(4)]
+        regress_r2[neuralname[neural_i]] = tmp
+
+    results = {}
+    results['regress_r2'] = regress_r2
+    results['feret_stats'] = stats
+
+    record = {}
+    record['spec'] = config
+    record['results'] = results
+    record['attachments'] = {}
+    record['loss'] = 1 - (view_test_rsq + test_accuracy)/2
+    record['status'] = 'ok'
+    return record
+
+## bandits ##
+@base.as_bandit()
+def FERETL3Bandit(template=None):
+    if template==None:
+        template = sp.l3_params
+    return scope.feret_bandit_evaluate(template)
+
+@base.as_bandit()
+def FERETL2Bandit(template=None):
+    if template==None:
+        template = sp.l2_params
+    return scope.feret_bandit_evaluate(template)
 
--- simffa_exp.py
+++ simffa_exp.py
@@ -3,6 +3,7 @@ import tabular as tb
 import hyperopt
 import simffa.simffa_experiments as sfexp
 import simffa.simffa_bandit 
+import simffa.simffa_feret
 
 def simffa_exp_main(dbname='simffa', randomSearch=False, label_set=1, shuf=False):
 	host = 'localhost'
@@ -40,132 +41,30 @@ def simffa_exp_main(dbname='simffa', randomSearch=False, label_set=1, shuf=False
 	return exp
 
 
-	
 
-# def startExp_main():
-# 	dbname = 'simffa_invar'
-# 	exp_keys = ['SimffaL1BanditRandom', 'SimffaL2BanditRandom', 'SimffaL3BanditRandom']
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.Random'] * 3
-# 	bandit_names = ['simffa.simffa_bandit.SimffaL1Bandit', 'simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_args_list = [{} for _i in range(3)]
-# 	bandit_kwargs_list = [{} for _i in range(3)]
-# 	bandit_algo_args_list=[() for _i in range(3)]
-# 	bandit_algo_kwargs_list=[{} for _i in range(3)]
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-	
-# 	return exp
+def simffa_exp_pruned(dbname='simffa_feret', randomSearch=False):
+	host = 'localhost'
+	port = 22334
+	nExps = 2 
 
+	bandit_names = ['simffa.simffa_feret.FERETL3Bandit', 'simffa.simffa_feret.FERETL2Bandit']
+	bandit_args_list = [() for _i in range(nExps)]
+	bandit_kwargs_list = [{} for _i in range(nExps)]
 
-# def simffaInvar():
-# 	dbname = 'simffa_invar'
-# 	# exp_keys = ['SimffaL1BanditRandom', 'SimffaL2BanditRandom', 'SimffaL3BanditRandom']
-# 	exp_keys = ['SimffaL2BanditRandom', 'SimffaL3BanditRandom']
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.Random'] * 2
-# 	# bandit_names = ['simffa.simffa_bandit.SimffaL1Bandit', 'simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_names = ['simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_args_list = [{} for _i in range(2)]
-# 	bandit_kwargs_list = [{} for _i in range(2)]
-# 	bandit_algo_args_list=[() for _i in range(2)]
-# 	bandit_algo_kwargs_list=[{} for _i in range(2)]
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-# 	return exp
+	if randomSearch:
+		bandit_algo_names = ['hyperopt.Random'] * nExps
+		bandit_algo_args_list=[() for _i in range(nExps)]
+		bandit_algo_kwargs_list=[{} for _i in range(nExps)]
+	else:
+		bandit_algo_names = ['hyperopt.TreeParzenEstimator'] * nExps
+		bandit_algo_args_list=[() for _i in range(nExps)]
+		bandit_algo_kwargs_list=[{'gamma':0.25, 'n_startup_jobs': 100} for _i in range(nExps)]
 
-# def simffaInvar_opt():
-# 	dbname = 'simffa_invar'
-	
-# 	exp_keys = ['SimffaL2BanditParzen', 'SimffaL3BanditParzen']
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.TreeParzenEstimator'] * 2
-	
-# 	bandit_names = ['simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_args_list = [{} for _i in range(2)]
-# 	bandit_kwargs_list = [{} for _i in range(2)]
+	exp_keys = ['simffa_feret_L3', 'simffa_feret_L2']
 
-# 	bandit_algo_args_list=[() for _i in range(2)]
-# 	bandit_algo_kwargs_list=[{'gamma':0.25, 'n_startup_jobs': 100} for _i in range(2)]
+	N = None
+	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
+                     exp_keys, N, bandit_args_list, bandit_kwargs_list, 
+                     bandit_algo_args_list, bandit_algo_kwargs_list)
+	return exp
 
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-	
-# 	return exp
-
-# def simffaInvar_opt_v1():
-# 	dbname = 'simffa_invar'
-	
-# 	exp_keys = ['V1BanditParzen']
-# 	nExps = 1
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.TreeParzenEstimator'] * nExps
-	
-# 	bandit_names = ['simffa.simffa_bandit.SimffaV1LikeBandit']
-# 	bandit_args_list = [{} for _i in range(nExps)]
-# 	bandit_kwargs_list = [{} for _i in range(nExps)]
-
-# 	bandit_algo_args_list=[() for _i in range(nExps)]
-# 	bandit_algo_kwargs_list=[{'gamma':0.25, 'n_startup_jobs': 100} for _i in range(nExps)]
-
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-# 	return exp
-
-# def simffaInvar_opt_shuffled():
-# 	dbname = 'simffa_invar_shuf'
-	
-# 	exp_keys = ['V1BanditParzen_shuf','L2BanditParzen_shuf', 'L3BanditParzen_shuf']
-# 	nExps = 3
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.TreeParzenEstimator'] * nExps
-	
-# 	bandit_names = ['simffa.simffa_bandit.SimffaL1Bandit', 'simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_args_list = [(1, True) for _i in range(nExps)]
-# 	bandit_kwargs_list = [{} for _i in range(nExps)]
-
-# 	bandit_algo_args_list=[() for _i in range(nExps)]
-# 	bandit_algo_kwargs_list=[{'gamma':0.25, 'n_startup_jobs': 100} for _i in range(nExps)]
-
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-	
-# 	return exp
-
-# def simffaInvar_opt_eye():
-# 	dbname = 'simffa_invar_eye'
-	
-# 	exp_keys = ['V1BanditParzen_eye','L2BanditParzen_eye', 'L3BanditParzen_eye']
-# 	nExps = 3
-# 	host = 'localhost'
-# 	port = 22334
-# 	bandit_algo_names = ['hyperopt.TreeParzenEstimator'] * nExps
-	
-# 	bandit_names = ['simffa.simffa_bandit.SimffaV1LikeBandit', 'simffa.simffa_bandit.SimffaL2Bandit', 'simffa.simffa_bandit.SimffaL3Bandit']
-# 	bandit_args_list = [{2} for _i in range(nExps)]
-# 	bandit_kwargs_list = [{} for _i in range(nExps)]
-
-# 	bandit_algo_args_list=[() for _i in range(nExps)]
-# 	bandit_algo_kwargs_list=[{'gamma':0.25, 'n_startup_jobs': 100} for _i in range(nExps)]
-
-# 	N = None
-# 	exp = sfexp.suggest_multiple_from_name(dbname, host, port, bandit_algo_names, bandit_names, 
-#                      exp_keys, N, bandit_args_list, bandit_kwargs_list, 
-#                      bandit_algo_args_list, bandit_algo_kwargs_list)
-	
-# 	return exp
